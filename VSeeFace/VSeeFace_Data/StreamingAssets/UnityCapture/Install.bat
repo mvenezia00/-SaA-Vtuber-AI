b8032ca03cf027bbae09bd30597b166336e3935d
Mario Venezia <mvenezia00@hotmail.it>
2023-05-03 00:15:50 +0200

    Aggiunti driver gestione audio e software gestione avatar

---
+++ VSeeFace/VSeeFace_Data/StreamingAssets/UnityCapture/Install.bat
@@ -0,0 +1,30 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges to register virtual camera driver...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+    echo Installing capture device named 'VSeeFaceCamera' ...
+    regsvr32 "VSeeFaceCamera32bit.dll" "/i:UnityCaptureName=VSeeFaceCamera"
+    regsvr32 "VSeeFaceCamera64bit.dll" "/i:UnityCaptureName=VSeeFaceCamera"
+    echo "Done"
+:--------------------------------------
